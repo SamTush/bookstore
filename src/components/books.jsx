678c36a18ba8072ded888102a0aa6d8363c9b3cf
Samson Tush <samo4421@icloud.com>
2023-04-07 22:28:44 +0300

    Add book list and form

--- src/components/books.jsx
+++ src/components/books.jsx
@@ -1,6 +1,17 @@
+import './books.scss';
+
 const Books = () => (
   <>
-    <h1>Hello books</h1>
+    <div className="container">
+      <div className="row">
+        <div className="col col-12 book-details">
+          <h1>Hello books</h1>
+        </div>
+        <div className="col col-12 add-books">
+          <h1>ADD NEW BOOK</h1>
+        </div>
+      </div>
+    </div>
   </>
 );
 
